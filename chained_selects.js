d7c63beb376d2394428c035633194c397ea03487
reid <reid@a06862d0-bf29-e17f-e302-fbc15f989f17>
2008-10-29 17:25:15 +0000

    extracking chained_selects from buzzadmin
    git-svn-id: http://svn.latimesdev.com/svn/repos/shared/javascript/chained_selects/trunk@13443 a06862d0-bf29-e17f-e302-fbc15f989f17

---
+++ chained_selects.js
@@ -0,0 +1,85 @@
+var ChainedSelects = Class.create();
+ChainedSelects.prototype = {
+  initialize: function(tree, form, selectNames, active, oncomplete) {
+    this.tree = tree;
+    this.names = selectNames.map(function(select) {
+      return select.size == undefined ? select : select[1]; // enumerable: [id, name]
+    });
+    this.active = active;
+    this.oncomplete = oncomplete;
+    this.defaults = [];
+    this.selection = [];
+    this.selects = selectNames.map(function(select) {
+      return select.size == undefined ? $(form)[select] : $(form)[select[0]]; // enumerable: [id, name]
+    });
+    this.populateSelect(0);
+  },
+  populateSelect: function(size) {
+    // get our information for our select
+    selected = false;
+    name = this.names[size];
+    tree = size == 0 ? this.tree : this.selection.inject(this.tree, function(tree, value) { 
+      return tree[value];
+    });
+
+    // move any active selection / remember it in defaults
+    active = this.active.shift();
+    this.defaults.push(active);
+    
+    // determine our options
+    options = [new Option("Choose "+name+"...",'choose',(active == undefined))];
+    for(i in tree) {
+      value = (size+1 == this.selects.size()) ? tree[i] : i;
+      options.push(new Option(i, value, (active == i)));
+      if(active == i) selected = value; 
+    }
+
+    // build our selects
+    select=this.selects[size];
+    select.options.length = 0;
+    for(i=0; i<options.size(); i++) {
+      select.options[i] = options[i];
+    }
+    select.disabled = false;    
+    this.chainSelect(size,select)
+
+    // if we have any active
+    if(selected && size < this.selects.size()-1) {
+      this.selection.push(selected);
+      this.populateSelect(size+1);
+    }
+
+    // do we need to call the oncomplete function?
+    this.doOnComplete();
+  },
+  manageSelects: function(level,value) {
+    // remove any levels above ours
+    size = this.selection.size();
+    if(size != level) {
+      for(i=size;i>level;i--) {
+        this.selection.pop();
+        this.selects[i].options.length = 0;
+        this.selects[i].disabled = true;
+      }
+    }
+    // build the next level up if a choice
+    if(value != 'choose' && level < this.selects.size()-1) {
+      size = this.selection.size() + 1;
+      this.selection.push(value);
+      this.populateSelect(size)
+    }
+    this.doOnComplete();
+  },
+  chainSelect: function(level,select) {
+    chain = this; // needed for closure
+    select.onchange = function(e) {
+      chain.manageSelects(level,Form.Element.getValue(e.target));
+    }
+  },
+  doOnComplete: function() {
+    var sel = this.selects[this.selects.size()-1]
+    if (this.oncomplete && !sel.disabled && sel.value != 'choose') {
+      this.oncomplete(sel.value);
+    }
+  }
+};
